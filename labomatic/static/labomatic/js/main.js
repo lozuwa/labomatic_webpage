ba29e7ac39a1bfedaa46ae641f6ec827e42c8b00
lozuwa <lozuwaucb@gmail.com>
2018-03-08 17:30:27 -0500

    redesigned frontend

--- labomatic/static/labomatic/js/main.js
+++ labomatic/static/labomatic/js/main.js
@@ -1,7 +1,3 @@
 jQuery(document).ready(function($){
-    $("moreExamplesButton").click(function(){
-      $post("https:localhost:8000/", {"hello": 1}, function(data, status){
-        alert("Data: " + data + "\nStatus: " + status);
-      });
-    });
+    
 });
